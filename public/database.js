939afb01e8159152dc448c39ca85c891a66cdaa5
james davies <james.davies92@icloud.com>
2020-03-14 16:13:30 +0000

    added new event functions to db.js file

--- public/database.js
+++ public/database.js
@@ -4,3 +4,23 @@ const indexedDB =
   window.webkitIndexedDB ||
   window.msIndexedDB ||
   window.shimIndexedDB;
+
+  let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  console.log("Woops! " + event.target.errorCode);
+};
